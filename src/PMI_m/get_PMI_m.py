b2c44cc9d22ac0ed745de9a74bbd62edf8d4166a
striker4267 <Strikerboy4267@gmail.com>
2025-04-15 22:13:45 +0100

    day two

--- src/PMI_m/get_PMI_m.py
+++ src/PMI_m/get_PMI_m.py
@@ -1,21 +1,22 @@
 import json
 
-def pmi_getter (c_code):
+def pmi_getter (c_code, tedata_lib):
     with open(r"src\T25.json", "r") as countries:
         countries = json.load(countries)
     
 
     #This returns a TE_scraper object with the data stored in the "series" attribute.
     if c_code != "US":
-        scraped = tedata.scrape_chart(url = f"https://tradingeconomics.com/{countries[c_code]}/manufacturing-pmi")
+        scraped = tedata_lib.scrape_chart(url = f"https://tradingeconomics.com/{countries[c_code]}/manufacturing-pmi")
     else:
-        scraped = ted.scrape_chart(url = "https://tradingeconomics.com/united-states/business-confidence")
+        scraped = tedata_lib.scrape_chart(url = "https://tradingeconomics.com/united-states/business-confidence")
 
     # Metadata is stored in the "metadata" attribute and the series is easily plotted 
     # using the "plot_series" method.
     #  
     try:
         scraped.export_data(filename = "mydata") #Will save to current wd as "my_data.xlsx"
+        print(scraped)
     except(PermissionError):
         print("Permission error: You likely have the excel file open")
 
--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import tedata as ted
 from PMI_m.get_PMI_m import pmi_getter
 
 def main(country):
-    pmi = pmi_getter(country)
+    pmi = pmi_getter(country, ted)
 
 
 
 if __name__ == "__main__":
-    main("US")
+    main("GB")
